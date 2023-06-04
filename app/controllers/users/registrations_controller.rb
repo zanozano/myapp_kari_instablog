5dc1d82b2cb62cf369b1958efb504e7557e92f3a
zanozano <zanozanozano@gmail.com>
2023-06-04 20:42:31 +0700

    terminado

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+    before_action :configure_account_update_params, only: [:update]
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
@@ -20,9 +21,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+def update
+  super do |resource|
+    if params[:user][:img_profile].present?
+      resource.update(img_profile: params[:user][:img_profile])
+    end
+  end
+end
 
   # DELETE /resource
   # def destroy
@@ -59,4 +64,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  private
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:img_profile])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  mount_uploader :img_profile, ImageUploader
   has_many :comments
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
